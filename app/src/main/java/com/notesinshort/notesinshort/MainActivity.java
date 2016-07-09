ac29f53ec3c0b96a0777a842e6aac542e3a03ac6
mayank26saxena <mayank26saxena@gmail.com>
2016-07-09 14:27:43 +0530

    implemented fab and camera

--- app/src/main/java/com/notesinshort/notesinshort/MainActivity.java
+++ app/src/main/java/com/notesinshort/notesinshort/MainActivity.java
@@ -2,15 +2,21 @@ package com.notesinshort.notesinshort;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.frosquivel.magicalcamera.MagicalCamera;
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
 
 /**
  * Created by mayank on 9/7/16.
@@ -18,26 +24,85 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     String TAG = MainActivity.class.getSimpleName();
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
     FirebaseUser user;
+    FloatingActionButton camera, choose_document;
+    FloatingActionMenu menu;
+    MagicalCamera magicalCamera;
+    ImageView imageView;
+    long unixTime;
+
+    //a regular quality, if you declare with 50 is a worst quality and if you declare with 4000 is the better quality
+    //only need to play with this variable (0 to 4000 ... or in other words, worst to better :D)
+
+    private int RESIZE_PHOTO_PIXELS_PERCENTAGE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAuth = FirebaseAuth.getInstance();
+        menu = (FloatingActionMenu) findViewById(R.id.menu);
+        camera = (FloatingActionButton) findViewById(R.id.camera);
+        choose_document = (FloatingActionButton) findViewById(R.id.choose_document);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        magicalCamera = new MagicalCamera(this, RESIZE_PHOTO_PIXELS_PERCENTAGE);
+
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+
+        choose_document.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open_document();
+            }
+        });
+
+        //Removed mAuth get instance code
 
         user = FirebaseAuth.getInstance().getCurrentUser();
 
         if (user == null) {
             navigateToLogin();
             Log.d(TAG, "User is null.");
-        }
-        else {
+        } else {
             Log.d(TAG, "User exists and is signed in.");
         }
+
+    }
+
+    public void openCamera() {
+
+        magicalCamera.takePhoto();
+        magicalCamera.selectedPicture("my_header_name");
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //call this method ever
+        magicalCamera.resultPhoto(requestCode, resultCode, data);
+
+        //with this form you obtain the bitmap
+        imageView.setImageBitmap(magicalCamera.getMyPhoto());
+
+        //if you need save your bitmap in device use this method
+        unixTime = System.currentTimeMillis() / 1000L;
+
+        if (magicalCamera.savePhotoInMemoryDevice(magicalCamera.getMyPhoto(), String.valueOf(unixTime), "NotesInShort", MagicalCamera.JPEG, true)) {
+            Toast.makeText(MainActivity.this, "The photo is save in device, please check this", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(MainActivity.this, "Sorry your photo dont write in devide, please contact with fabian7593@gmail and say this error", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void open_document() {
 
     }
 
