119e5e496332f228cbea16600505b48fb0716c52
mayank26saxena <mayank26saxena@gmail.com>
2016-07-09 12:41:15 +0530

    login signup complete

--- app/src/main/java/com/notesinshort/notesinshort/MainActivity.java
+++ app/src/main/java/com/notesinshort/notesinshort/MainActivity.java
@@ -1,16 +1,47 @@
 package com.notesinshort.notesinshort;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * Created by mayank on 9/7/16.
  */
 public class MainActivity extends AppCompatActivity {
 
+    String TAG = MainActivity.class.getSimpleName();
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    FirebaseUser user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user == null) {
+            navigateToLogin();
+            Log.d(TAG, "User is null.");
+        }
+        else {
+            Log.d(TAG, "User exists and is signed in.");
+        }
+
+    }
+
+    private void navigateToLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
